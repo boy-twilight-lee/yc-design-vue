a1be3e0df12f2936f7ec4036bfe17859fc677511
李银超 <2429162154@qq.com>
2025-08-29 14:00:06 +0800

    [perf]还原layout的设置

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import ArcoIcon from '@arco-design/web-vue/es/icon';
+import '@arco-design/web-vue/dist/arco.css';
 import YcDesign from '@/components';
 import '@/components/index.less';
 
