91702dc0a6868f695656136e62ff72229ef5dbfa
李银超 <2429162154@qq.com>
2025-09-01 10:52:55 +0800

    [fix]修复部分组件主题异常的问题

--- src/views/dev.vue
+++ src/views/dev.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="test"></div>
+  <div class="test">
+    <yc-select :options="[1, 2, 3]" placeholder="请选择" :bordered="false">
+    </yc-select>
+    <a-select placeholder="请选择" :bordered="false">
+      <template #header> dsads </template>
+      <template #footer> dsads </template>
+      <a-option disabled> 1 </a-option>
+    </a-select>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { useDark } from '@vueuse/core';
-document.body.setAttribute('arco-theme', 'dark');
-const isDark = useDark({
-  selector: 'body',
-  attribute: 'yc-design-theme',
-  valueDark: 'dark',
-  valueLight: 'light',
-});
-isDark.value = true;
+import { Notification } from '@arco-design/web-vue';
+// document.body.setAttribute('arco-theme', 'dark');
+// const isDark = useDark({
+//   selector: 'body',
+//   attribute: 'yc-design-theme',
+//   valueDark: 'dark',
+//   valueLight: 'light',
+// });
+// isDark.value = true;
 </script>
 
 <style lang="less" scoped>
